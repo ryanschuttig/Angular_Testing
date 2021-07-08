7d831df28c2a2a2c445c273c63fc40db41072150
Ryan Schuttig <ryan.schuttig@capco.com>
2021-07-08 12:10:26 -0400

    added course testing methods

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,5 +1,6 @@
 import {CalculatorService} from './calculator.service';
 import {LoggerService} from './logger.service';
+import {TestBed} from '@angular/core/testing';
 
 
 describe('CalculatorService', () => {
@@ -13,7 +14,14 @@ describe('CalculatorService', () => {
 
         loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
 
-        calculator = new CalculatorService(loggerSpy);
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                {provide: LoggerService, useValue: loggerSpy}
+            ]
+        });
+
+        calculator = TestBed.get(CalculatorService);
 
     });
 
---
+++ src/app/courses/services/courses.services.spec.ts
@@ -0,0 +1,75 @@
+import { TestBed } from "@angular/core/testing";
+import { CoursesService } from "./courses.service";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { COURSES } from "../../../../server/db-data";
+
+
+describe("CoursesService", () => {
+
+    let coursesService: CoursesService,
+        httpTestingController: HttpTestingController;
+
+    beforeEach(() => {
+
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [
+                CoursesService,
+            ]
+        });
+
+        coursesService = TestBed.get(CoursesService);
+        httpTestingController = TestBed.get(HttpTestingController);
+
+    });
+    
+    it('should retrieve all courses', () => {
+
+        coursesService.findAllCourses()
+            .subscribe(courses => {
+
+                expect(courses).toBeTruthy('No courses returned');
+                
+                expect(courses.length).toBe(12,
+                    "incorrect number of courses");
+
+                const course = courses.find(course => course.id == 12);
+
+                expect(course.titles.description).toBe(
+                    "Angular Testing Course");
+
+            });
+
+        const req = httpTestingController.expectOne('/api/courses');
+
+        expect(req.request.method).toEqual("GET");
+
+        req.flush({payload: Object.values(COURSES)});
+
+    });
+
+
+        it('should find a course by id', () => {
+
+            coursesService.findCourseById(12)
+                .subscribe(course => {
+    
+                    expect(course).toBeTruthy();
+                    expect(course.id).toBe(12);
+    
+                });
+    
+            const req = httpTestingController.expectOne('/api/courses/12');
+    
+            expect(req.request.method).toEqual("GET");
+    
+            req.flush(COURSES[12]);
+
+    });
+
+    afterEach(() => {
+
+        httpTestingController.verify();
+    });
+
+});
