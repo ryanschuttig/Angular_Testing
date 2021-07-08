63dbd5bfd55139376c670481a91f2f3a24978a9f
Ryan Schuttig <ryan.schuttig@capco.com>
2021-07-08 10:38:43 -0400

    isolated tests

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -4,29 +4,41 @@ import {LoggerService} from './logger.service';
 
 describe('CalculatorService', () => {
 
+    let calculator: CalculatorService,
+        loggerSpy: any;
+
+    beforeEach(()=> {
+
+        console.log("Calling beforeEach");
+
+        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+
+        calculator = new CalculatorService(loggerSpy);
+
+    });
 
     it('should add two numbers', () => {
 
-        const logger = jasmine.createSpyObj('LoggerService', ["log"]);
-
-        const calculator = new CalculatorService(logger);
+        console.log("add test");
 
         const result = calculator.add(2, 2);
 
         expect(result).toBe(4);
 
-        expect(logger.log).toHaveBeenCalledTimes(1);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
     });
 
 
     it('should subtract two numbers', () => {
 
-        const calculator = new CalculatorService(new LoggerService());
+        console.log("subtract test");
 
         const result = calculator.subtract(2, 2);
 
         expect(result).toBe(0);
+
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
         
     });
 
